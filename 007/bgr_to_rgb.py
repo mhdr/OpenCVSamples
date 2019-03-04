6ea33245edcd96bbcc08c5f531b560332ddbf333
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2019-03-04 11:31:10 +0330

    007

--- 007/bgr_to_rgb.py
+++ 007/bgr_to_rgb.py
@@ -7,5 +7,11 @@ cv2.imshow("image", image)
 fixed = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 cv2.imshow("image2", fixed)
 
+image_gray = cv2.imread("cow.jpg", cv2.IMREAD_GRAYSCALE)
+cv2.imshow("image3", image_gray)
+
+image_gray2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("image4", image_gray2)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
